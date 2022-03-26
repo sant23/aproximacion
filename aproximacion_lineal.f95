ddf22b7734072c282309e6b9d9518378575ac74c
sant23 <santilg211203@gmail.com>
2022-03-26 12:21:04 +0100

    trabajo terminado

--- aproximacion_lineal.f95
+++ aproximacion_lineal.f95
@@ -15,7 +15,7 @@ module aproximacion
         m = size(x)-1
         
         
-        write(*,*)'mnmnmn',n,m
+        write(*,*)'n----m',n,m
         do l=0, m
             do k=0, m 
                 s2 = 0.d0 
@@ -44,22 +44,31 @@ module aproximacion
     end subroutine minimos_cuadrados
 
 
-    subroutine error(Xn,Yn,px,X)
+    subroutine error(Xn,Yn,px,X,e)
         real(8), intent(in)::Xn(:),Yn(:),X(:)
-        real(8), intent(out)::px
+        real(8), intent(out)::e,px(:)
         integer::m,n,i
-        real(8)::s  
+        real(8)::s,s2  
 
         n=size(xn)
         m= size(x)-1
-        s=0.d0
-
-        do i=0, m
-            s= s + X(i+1)*xn(i+1)**i 
-            px=s 
-        enddo 
-        write(*,*)'-------PX-------'
-        write(*,*)PX 
+        
+        
+       ! e=0.d0
+        !do i=1, n
+         !       s=0.d0
+        !        do l=0, m 
+        !            s= s + X(l+1)*xn(i)**(l) 
+        !            px(i)=s
+         !           ENDDO
+                
+        !        e = e + (yn(i)-s)**2
+       ! enddo
+        
+        !write(*,*)'-------PX-------'
+        !write(*,*)px
+        !write(*,*)'------ERROR-----'
+        !write(*,*) e
 
     end subroutine error 
 
--- principal.f95
+++ principal.f95
@@ -3,8 +3,8 @@ program minimos
     use aproximacion 
 
     implicit none
-    real(8), allocatable :: Xn(:), Yn(:), A(:,:), B(:), X(:)
-    real(8)::px
+    real(8), allocatable :: Xn(:), Yn(:), A(:,:), B(:), X(:),px(:)
+    real(8)::e,s 
     character(15)::fichero 
     integer :: i,l,j,k,n,m
     
@@ -20,7 +20,7 @@ program minimos
    
     read(10,*) n
    
-    allocate (Xn(n),Yn(n), A(m+1,m+1), B(m+1), X(m+1))
+    allocate (Xn(n),Yn(n), A(m+1,m+1), B(m+1), X(m+1),px(n))
 
     do i=1, n   
         read(10,*) Xn(i), Yn(i)
@@ -29,10 +29,10 @@ program minimos
     call minimos_cuadrados(a,b,x,xn,yn)
 
     
-     write(*,*) '-------b--------'
-     write(*,*)   b  
-    write(*,*) '-------a--------'
-    write(*,*) a 
+    ! write(*,*) '-------b--------'
+   !  write(*,*)   b  
+    !write(*,*) '-------a--------'
+    !write(*,*) a 
 
 
     !write (*,*) '------vector xn-------'
@@ -40,7 +40,20 @@ program minimos
     !write (*,*) '------vector yn-------'
     !write(*,*) yn
     
-    call error(Xn,Yn,px,x)
+    call error(Xn,Yn,px,x,e)
+
+    open(unit=14,file='salidam.csv')
+    write(14,*) 'x,   y,    px'
+    e=0.d0
+    do i=1, n 
+        s=0.d0
+        do l=0, m 
+            s = s + x(l+1)*xn(i)**l 
+        enddo 
+        write(14,*) xn(i), ',', yn(i), ',', s 
+        e = e + (yn(i)-s)**2 
+    enddo 
+    write(*,*)m, e
 
 
 end program  minimos
